15598067b7af1887a70e4707e26765716399a7d1
Melqui <melqui.sodre15@gmail.com>
2022-06-21 11:23:08 -0300

    configuration provider apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import { useEffect } from "react"
-import { client } from "../services/apollo"
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 
 const GET_LESSONS_QUERY = gql`
   query {
@@ -12,13 +10,9 @@ const GET_LESSONS_QUERY = gql`
 `
 
 export default function Home() {
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
+  const { data } = useQuery(GET_LESSONS_QUERY)
+
+  console.log(data)
 
   return (
     <h1 className="text-3xl font-bold underline">Hello Ignite Lab</h1>
