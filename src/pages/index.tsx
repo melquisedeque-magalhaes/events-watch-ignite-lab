0d8a20f67ffc7c337c230daab3abe8c159206193
Melqui <melqui.sodre15@gmail.com>
2022-06-24 01:52:20 -0300

    ajuste no sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import { Sidebar } from "../components/SiderBar";
+import Sidebar  from "../components/SideBar";
 
 export default function Home() {
 
